8191e39e68dbaa558c244c766d23b877f402361a
Mohssin19 <mohssin.a@outlook.com>
2024-05-23 14:24:17 +0100

    fizzbuzz_test1_passed_moved_logic_higher

--- FizzBuzz/FizzbuzzService.cs
+++ FizzBuzz/FizzbuzzService.cs
@@ -4,9 +4,9 @@
     {
         public string IsFizzbuzz(int number)
         {
-            return number % 3 == 0 ? "Fizz" : 
-                   number % 5 == 0 ? "Buzz" : 
-                   number % 15 == 0 ? "Fizzbuzz" :
+            return number % 15 == 0 ? "Fizzbuzz" :
+                   number % 3 == 0 ? "Fizz" : 
+                   number % 5 == 0 ? "Buzz" :                    
                    number.ToString();
         }
     }
