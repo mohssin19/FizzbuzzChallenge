711951a58d172cae8a13d794053a310f4a59bc88
Mohssin19 <mohssin.a@outlook.com>
2024-05-23 14:23:13 +0100

    fizzbuzz_test1_still_failed_after_logic_added

--- FizzBuzz/FizzbuzzService.cs
+++ FizzBuzz/FizzbuzzService.cs
@@ -6,6 +6,7 @@
         {
             return number % 3 == 0 ? "Fizz" : 
                    number % 5 == 0 ? "Buzz" : 
+                   number % 15 == 0 ? "Fizzbuzz" :
                    number.ToString();
         }
     }
